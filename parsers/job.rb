08c62d32c52aa51da18b0300b0f236e3208a25ce
Aadesh <aadesh@namespace.jp>
2019-08-04 13:34:36 +0545

    complete scraper

---
+++ parsers/job.rb
@@ -0,0 +1,27 @@
+# url = "https://www.indeed.com/rc/clk?jk=dcd418f5e22b6083&fccid=05d6cb8b919478a9&vjs=3"
+
+# content = open url
+
+nokogiri = Nokogiri::HTML content
+
+title = nokogiri.xpath('//h3[contains(@class, "jobsearch-JobInfoHeader-title")]').text
+company = nokogiri.xpath('///span[contains(@class, "jobsearch-JobInfoHeader-companyName")]/a').text
+location = nokogiri.xpath('//div[contains(@class, "jobsearch-JobInfoHeader-companyLocation")]').text
+job_description = nokogiri.xpath('//div[@id="jobDescriptionText"]').text
+job_type = nokogiri.xpath('//p[contains(text(), "Job Type")]').text
+job_location = nokogiri.xpath('//p[contains(text(), "Location")]').text
+scraped_at = Time.now.utc
+
+job = {}
+
+job['queryString'] = page['vars']['query']
+job['title'] = title
+job['company'] = company
+job['location'] = location.empty? ? job_location : location
+job['job_description'] = job_description
+job['job_type']　= job_type
+job['scraped_at'] = scraped_at
+
+job['_collection'] = "jobs"
+
+outputs << job
--- parsers/listings.rb
+++ parsers/listings.rb
@@ -4,15 +4,17 @@ SITE_URL = "https://www.indeed.com".freeze
 
 jobs = nokogiri.xpath('//a[contains(@class, "jobtitle")]')
 
+job_urls = []
+
 jobs.each do |job|
 	link = job.xpath('./@href')&.text
 	
 	url = SITE_URL + link
-
+	job_urls << url
 	if url =~ /\Ahttps?:\/\//i
 		pages << {
 			url: url,
-			page_type: 'jobs',
+			page_type: 'job',
 			fetch_type: 'browser',
 			force_fetch: true,
 			vars: {
